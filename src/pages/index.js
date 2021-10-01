5d46ae5089aebfd86c734cdac0912568032f7cda
Timboow <tim@yourcreativebrand.nl>
2021-10-01 16:48:54 +0200

    more animation testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { CopyBlock, nord } from 'react-code-blocks'
 import Hero from '../components/Hero'
+import { useSpring, animated as a } from "react-spring";
+ 
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
 
 
+export default () => {
+
+  const animatedStyle =  useSpring({
+    delay: 1000,
+    from:{opacity:0, marginTop:'-50px'},
+    to:{opacity:1, marginTop:'0px'},
+  })
+  
 
 
-export default () => (
-  <div className="container py-5">
+  return (
+    <div className="container py-5">
     <div className="row mb-5">
       <div className="col-md-8 text-center mx-auto intro">
         <Hero/>
+        <a.div style={animatedStyle}>
         <p>Terrand is an <strong>oracle</strong> smart contract on Terra Luna to get distributed randomness beacon. <strong>Verifiable, unpredictable and unbiased random numbers</strong> as a service.</p>
-        <a className="btn btn-primary">Learn more</a>
+        </a.div>
+        <a.a style={animatedStyle} className="btn btn-primary">Learn more</a.a>
       </div>      
     </div>
     <div className="row info-block">
@@ -19,21 +33,21 @@ export default () => (
           <div className="align-self-center w-100">
           <h2>Add randomness</h2>
         <p>{'You can get latest randomness from https://drand.cloudflare.com/public/latest or by round https://drand.cloudflare.com/public/{round}'}</p>
-        <CopyBlock
-       text={`{
+        <CodeMirror
+      value={`{
         "drand":{
             "round": u64,
             "previous_signature": Binary,
             "signature": Binary
         }
-      }      
-      `}
-       language={'javascript'}
-       showLineNumbers={true}
-      //  startingLineNumber={1}
-       theme={nord}
-       wrapLines
-        />
+    }`}
+      height="200px"
+      options={{
+        theme: 'monokai',
+        keyMap: 'sublime',
+        mode: 'javascript',
+      }}
+    />
           </div>
       </div>
       <div className="col-md-6 text-center">
@@ -47,50 +61,13 @@ export default () => (
       <div className="col-md-6">
         <h2>Get last randomness</h2>
         <p>Query:</p>
-        <CopyBlock
-       text={`{
-        "latest_drand": {}
-    }`}
-       language={'javascript'}
-       showLineNumbers={true}
-      //  startingLineNumber={1}
-       theme={nord}
-       wrapLines
-        />
-        <p>Result:</p>
-        <CopyBlock
-       text={`{
-        "height": string,
-        "result": {
-          "round": u64,
-          "randomness": Binary,
-          "worker": HumanAddr
-        }
-      }`}
-       language={'javascript'}
-       showLineNumbers={true}
-      //  startingLineNumber={1}
-       theme={nord}
-       wrapLines
-        />
+       
+       
+    
   <p>Result example:</p>
-<CopyBlock
-       text={`{
-        "height": "2439975",
-        "result": {
-          "round": 573531,
-          "randomness": "yTBW2ubloeFa+ZRh08Jt+4jVQHHGMX4s3j8mTYKc3oQ=", // This is the randomness hash from drand
-          "worker": "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v" // Worker is the address of the account who added this random
-        }
-      }`
-    }
-       language={'javascript'}
-       showLineNumbers={true}
-      //  startingLineNumber={1}
-       theme={nord}
-       wrapLines
-        />
+
       </div>
     </div>
   </div>
-)
+  )
+  }
